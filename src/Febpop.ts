48da18c6109c1980669977783c4dfb2b0b7d4579
Nicholas Yip <nicholas.yip@justincase-tech.com>
2020-02-14 18:19:07 +0900

    Add `on` and `emit`

--- src/Febpop.ts
+++ src/Febpop.ts
@@ -9,6 +9,8 @@ const defaultOptions = {
 
 /** @internal */
 export class Febpop {
+  private readonly pendingListeners = new Array<(socket: Socket) => void>()
+  private readonly emissionBuffer = new Set<() => void>()
   private socket?: Socket
 
   constructor(
@@ -23,8 +25,32 @@ export class Febpop {
   private get delegate() {
     if (!this.socket) {
       const options = !this.options ? defaultOptions : { ...defaultOptions, ...this.options }
-      this.socket = this.uri === null ? io(options) : io(this.uri, options)
+      const socket = this.uri === null ? io(options) : io(this.uri, options)
+      this.socket = socket
+
+      this.pendingListeners.forEach(f => f(socket))
+      socket.on('connect', () => this.emissionBuffer.forEach(f => f()))
+      socket.connect()
     }
     return this.socket
+  }
+
+  on = (event: string) => (action: (argument: any) => void) => {
+    if (!this.socket) {
+      this.pendingListeners.push(delegate => delegate.on(event, action))
+    } else {
+      this.delegate.on(event, action)
+    }
+  }
+
+  emit = (event: string) => (argument: any) => {
+    const socket = this.delegate
+
+    const emission = () => socket.emit(event, argument, () => this.emissionBuffer.delete(emission))
+    this.emissionBuffer.add(emission)
+
+    if (socket.connected) {
+      emission()
+    }
   }
 }
